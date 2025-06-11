355b14fb24aa9cdfac6d951fc5fe610899ccdf59
Sjuniorr6 <sjuniorr6@gmail.com>
2025-06-11 12:17:05 -0300

    mapa

--- service/urls.py
+++ service/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
+from . import views
 from .views import get_t42_data, MapaView2, get_devices_data, ultima_posicao_veiculos, trafegus_veiculos
 from django.conf import settings
 from django.conf.urls.static import static
 import os
 
 urlpatterns = [
-    path('api/get_t42_data/', get_t42_data, name="get_t42_data"),
+    path('api/get_t42_data/', views.get_t42_data, name="get_t42_data"),
     path("mapa2/", MapaView2.as_view(), name="mapa2"),
     path('ultima-posicoes/', ultima_posicao_veiculos, name='ultima-posicoes'),
     path('get_devices_data/', get_devices_data, name='get_devices_data'),
-    path('trafegus_veiculos/', trafegus_veiculos, name="trafegus_veiculos"),
-    path('service/trafegus_veiculos/', trafegus_veiculos, name="trafegus_veiculos"),
+    path('trafegus_veiculos/', views.trafegus_veiculos, name="trafegus_veiculos"),
+    path('service/trafegus_veiculos/', views.trafegus_veiculos, name="trafegus_veiculos"),
     path('remove-alert/', views.remove_alert, name='remove_alert'),
 ]
 if settings.DEBUG:
